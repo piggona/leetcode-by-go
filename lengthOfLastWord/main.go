47a58edb3ba50c7ff1e529395f5f8c891f6232b1
Yuanhao Jin <haohaojin95@gmail.com>
2019-10-17 09:04:10 +0800

    leetcode 58

---
+++ lengthOfLastWord/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func lengthOfLastWord(s string) int {
+	result := 0
+	pos := len(s) - 1
+	for pos >= 0 && s[pos] == ' ' {
+		pos--
+	}
+	for pos >= 0 && s[pos] != ' ' {
+		result++
+		pos--
+	}
+	return result
+}
+
+func main() {
+	input := ""
+	fmt.Println(lengthOfLastWord(input))
+
+}
