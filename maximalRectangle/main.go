5d6077df2b225cec0122adcd04e0efe5161864d8
Yuanhao Jin <haohaojin95@gmail.com>
2019-11-23 16:27:48 +0800

    leetcode 85

--- maximalRectangle/main.go
+++ maximalRectangle/main.go
@@ -48,13 +48,77 @@ func getMax(nums ...int) int {
 	return max
 }
 
-func main() {
-	input := [][]byte{
-		{'1', '0', '1', '0', '0'},
-		{'1', '0', '1', '1', '1'},
-		{'1', '1', '1', '1', '1'},
-		{'1', '0', '0', '1', '0'},
+func largestRectangleAreaStack(height []int) int {
+	height = append(height, 0)
+	stack := []int{}
+	i := 0
+	max := 0
+	for i < len(height) {
+		if len(stack) == 0 || height[i] > height[stack[len(stack)-1]] {
+			stack = append(stack, i)
+			i++
+		} else {
+			temp := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			var val int
+			if len(stack) == 0 {
+				val = height[temp] * i
+			} else {
+				val = height[temp] * (i - stack[len(stack)-1] - 1)
+			}
+			max = getMax(max, val)
+		}
+	}
+	return max
+}
+
+func maximalRectangleStack(matrix [][]byte) int {
+	max := 0
+	height := make([]int, len(matrix[0]))
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix[0]); j++ {
+			if matrix[i][j] == '0' {
+				height[j] = 0
+			} else {
+				height[j] += 1
+			}
+		}
+		max = getMax(max, largestRectangleAreaStack(height))
+	}
+	return max
+}
+
+func getHeight(matrix [][]byte) [][]int {
+	count := make([][]int, len(matrix))
+	ok := make([][]bool, len(matrix))
+	for i := 0; i < len(matrix); i++ {
+		count[i] = make([]int, len(matrix[0]))
+		ok[i] = make([]bool, len(matrix[0]))
 	}
 
-	fmt.Println(maximalRectangle(input))
+	for i := len(matrix) - 1; i >= 0; i-- {
+		for j := 0; j < len(matrix[0]); j++ {
+			for k := i; k < len(matrix); k++ {
+				if matrix[i][j] == '1' {
+					if ok[k][j] == false {
+						count[k][j] += 1
+					}
+				} else {
+					ok[k][j] = true
+				}
+			}
+		}
+	}
+	return count
+}
+
+func main() {
+	input := [][]byte{
+		{'1', '1', '1', '1', '0'},
+		{'1', '0', '1', '1', '1'},
+		{'1', '1', '1', '1', '1'},
+		{'1', '0', '1', '1', '0'},
+	}
+
+	fmt.Println(maximalRectangleStack(input))
 }
