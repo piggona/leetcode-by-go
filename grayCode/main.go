48e1e26cb7d71b1ea94aa0ece47637337b854396
Yuanhao Jin <haohaojin95@gmail.com>
2019-11-28 08:57:40 +0800

    leetcode 89

---
+++ grayCode/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+)
+
+func grayCode(n int) []int {
+	size := pow(n) - 1
+	numMap := make(map[int]bool)
+	result := []int{0}
+	numMap[0] = true
+	is, result := getCode(n, size, numMap, result)
+	if is {
+		return result
+	}
+	return nil
+}
+
+func getCode(n, size int, numMap map[int]bool, result []int) (bool, []int) {
+	if size == 0 {
+		return true, result
+	}
+	for i := 0; i < n; i++ {
+		temp := result[len(result)-1]
+		divide := getDivide(temp, n)
+		pos := divide[i]
+		new := 0
+		if pos == 1 {
+			new = temp - pow(i)
+		} else {
+			new = temp + pow(i)
+		}
+
+		if !numMap[new] {
+			list := append(result, new)
+			newMap := numMap
+			newMap[new] = true
+			is, res := getCode(n, size-1, newMap, list)
+			if is {
+				return true, res
+			}
+		}
+	}
+	return false, nil
+}
+
+func getDivide(num, n int) []int {
+	result := []int{}
+	for i := 0; i < n; i++ {
+		temp := num % 2
+		result = append(result, temp)
+		num = num / 2
+	}
+	return result
+}
+
+func pow(n int) int {
+	result := 1
+	for i := 0; i < n; i++ {
+		result *= 2
+	}
+	return result
+}
+
+func main() {
+	input := 3
+	fmt.Println(grayCode(input))
+}
