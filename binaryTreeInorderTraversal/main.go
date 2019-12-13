432423d254867efc863d295b596739aa6c7280aa
Yuanhao Jin <haohaojin95@gmail.com>
2019-12-13 09:40:21 +0800

    BST,inorder rraversal,preorder traversal,postorder traversal,Morris Tree

--- binaryTreeInorderTraversal/main.go
+++ binaryTreeInorderTraversal/main.go
@@ -48,9 +48,103 @@ func inorderTraversalStack(root *TreeNode) []int {
 	return result
 }
 
+func inorderTraversalMorris(root *TreeNode) []int {
+	result := []int{}
+	cur := root
+	for cur != nil {
+		if cur.Left == nil {
+			result = append(result, cur.Val)
+			cur = cur.Right
+		} else {
+			temp := getFrontier(cur)
+			if temp.Right == cur {
+				temp.Right = nil
+				result = append(result, cur.Val)
+				cur = cur.Right
+			} else if temp.Right == nil {
+				temp.Right = cur
+				cur = cur.Left
+			}
+		}
+	}
+	return result
+}
+
+func preorderTraversalMorris(root *TreeNode) []int {
+	result := []int{}
+	cur := root
+	for cur != nil {
+		if cur.Left == nil {
+			result = append(result, cur.Val)
+			cur = cur.Right
+		} else {
+			temp := getFrontier(cur)
+			if temp.Right == cur {
+				temp.Right = nil
+				cur = cur.Right
+			} else if temp.Right == nil {
+				temp.Right = cur
+				result = append(result, cur.Val)
+				cur = cur.Left
+			}
+		}
+	}
+	return result
+}
+
+func postorderTraversalMorris(root *TreeNode) []int {
+	result := []int{}
+	dump := &TreeNode{Left: root}
+	cur := dump
+	for cur != nil {
+		if cur.Left == nil {
+			cur = cur.Right
+		} else {
+			temp := getFrontier(cur)
+			if temp.Right == nil {
+				temp.Right = cur
+				cur = cur.Left
+			} else if temp.Right == cur {
+				reverse(cur.Left, temp)
+				for temp != cur.Left {
+					result = append(result, temp.Val)
+					temp = temp.Right
+				}
+				result = append(result, temp.Val)
+				temp.Right = nil
+				cur = cur.Right
+			}
+		}
+	}
+	return result
+}
+
+func reverse(from, to *TreeNode) {
+	if from == to {
+		return
+	}
+	x, y := from, from.Right
+	var z *TreeNode
+	for x != to {
+		z = y.Right
+		y.Right = x
+		x.Right = z
+		x = y
+		y = z
+	}
+}
+
+func getFrontier(root *TreeNode) *TreeNode {
+	result := root.Left
+	for result.Right != nil && result.Right != root {
+		result = result.Right
+	}
+	return result
+}
+
 func main() {
 	input := []int{1, -1, 2, 3}
-	fmt.Println(inorderTraversalStack(getTree(input)))
+	fmt.Println(postorderTraversalMorris(getTree(input)))
 }
 
 func getTree(input []int) *TreeNode {
